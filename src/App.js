4a473aff7a36e9f09dc0ee735c19bcdb95b9c16f
Brindashree C B <brindashree@users.noreply.github.com>
2021-05-11 18:57:33 +0530

    display movie list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Movie from './components/Movie';
+ 
+
+
+const api = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9068210e8118347b031585ea0e418dd4&page=1`;
+
+const search_api = "https://api.themoviedb.org/3/search/movie?api_key=9068210e8118347b031585ea0e418dd4&query="
+
 
 function App() {
+
+  
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    getMovies(api);
+  }, []);
+
+  const getMovies = (apikey) => {
+    fetch(apikey)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(search_api + searchTerm);
+      setSearchTerm('');
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <header>
+          <h1 className="head">Movie Flix</h1>
+          <form onSubmit={handleOnSubmit}>
+             <input
+                className="search"
+                type="search"
+                placeholder="search..."
+                value={searchTerm}
+                onChange={handleOnChange}
+               />
+          </form>
+        </header>
+        <div className="movie-container">
+      
+                 {movies.length > 0 && movies.map((movie) => {
+                     return (
+                       <Movie {...movie} key={movie.id}/>
+                     )
+                 })}
+        </div>
+      </div>
   );
 }
 
---
+++ src/components/Movie.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const image_api = "https://image.tmdb.org/t/p/w1280";
+
+const setVoteClass = (vote) => {
+    if (vote >= 8) {
+        return 'green';
+    }
+    else if (vote >= 6) {
+        return 'orange';
+    }
+    else {
+        return 'red';
+    }
+}
+
+const Movie = ({title,poster_path,vote_average}) => {
+    return (
+        <div className="movie">
+            <img src={poster_path ? image_api + poster_path :"https://images.unsplash.com/photo-1535016120720-40c646be5580?ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8M3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                } alt={title} />
+            <div className="movie-info">
+                <h6>{title}</h6>
+                
+                <span className={`tag ${setVoteClass(vote_average)}`} >{vote_average}</span>
+                
+            </div>
+            
+        </div>
+    );
+}
+ 
+export default Movie;
